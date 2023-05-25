809bbb866a0810d325a00f15c3e3bcb1c6fe04f7
ngmichel20 <ngmichel20@hotmail.com>
2023-05-25 14:53:30 +0200

    first commit

---
+++ backend/src/main/java/com/ngmichel/kinefilmsv2/KineFilmsV2Application.java
@@ -0,0 +1,13 @@
+package com.ngmichel.kinefilmsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KineFilmsV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KineFilmsV2Application.class, args);
+  }
+
+}
